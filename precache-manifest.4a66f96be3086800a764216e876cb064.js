102b09dbe0c5927df0988133174a7ee94b7ac1b2
leangseu <kimleangseu@gmail.com>
2020-02-22 00:04:52 -0500

    No commit message

---
+++ precache-manifest.4a66f96be3086800a764216e876cb064.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debce3586cf4f8bb71b46bf1d1aee50d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "564ef9d062d5d7b8922b",
+    "url": "/static/css/main.d8aa8af2.chunk.css"
+  },
+  {
+    "revision": "f22ea5a4ab5040706a8f",
+    "url": "/static/js/2.0ea75aa6.chunk.js"
+  },
+  {
+    "revision": "e50876694b9a73c7f9eb95edba290e5b",
+    "url": "/static/js/2.0ea75aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564ef9d062d5d7b8922b",
+    "url": "/static/js/main.242bd624.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "c09695652c0b9d95f90a2f21db40ac7b",
+    "url": "/static/media/AutomaText.c0969565.gif"
+  },
+  {
+    "revision": "e7ff4c6e6a91d0d70e084fb7197095b4",
+    "url": "/static/media/Mockito.e7ff4c6e.png"
+  },
+  {
+    "revision": "e058571fd7f48d145cd9c3362914e075",
+    "url": "/static/media/MusicMash.e058571f.gif"
+  },
+  {
+    "revision": "744aa20fdd2e7beadaaa4b4754fd3d4d",
+    "url": "/static/media/messenger.744aa20f.gif"
+  },
+  {
+    "revision": "0b9fbcfb17fa1c3ef01732389cb73360",
+    "url": "/static/media/profile.0b9fbcfb.jpg"
+  },
+  {
+    "revision": "b8b41f33faad0a81f6350078f85f4c9a",
+    "url": "/static/media/timberman.b8b41f33.gif"
+  }
+]);
